16892d103cf2673aa965c647009f808d862e4cad
KacperBiedka <kacper.biedka.00@gmail.com>
2020-11-07 15:18:47 +0100

    feature: Table filter and pagination components

---
+++ src/modules/search/components/ResultsTable/TableOptions/TableOptions.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+import Input from "../../../../../components/Input/Input";
+import classes from "./TableOptions.module.scss";
+
+// Potentially this compoennt could contain more options related to the table content
+
+interface IOptionsProps {
+    perPage: number,
+    updateCallback: (value: React.ReactText) => void
+}
+
+const TableOptions: FC<IOptionsProps> = ({ perPage, updateCallback }) => {
+  return (
+    <div className={classes.perPageInputWrapper}>
+      <label htmlFor="perPage-input">Entries per page</label>
+      <Input
+        data-test="perPage-input"
+        value={perPage}
+        changeCallback={(value) => updateCallback(value)}
+        id="perPage-input"
+        type="number"
+      />
+    </div>
+  );
+};
+
+export default TableOptions;
---
+++ src/modules/search/components/ResultsTable/TablePagination/TablePagination.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import classes from "./TablePagination.module.scss";
+import { v4 as uuid } from "uuid";
+
+interface IPaginationProps {
+    paginationNumbers: number[],
+    updateCurrentPage: (number: number) => void,
+    currentPage: number 
+}
+
+const TablePagination: FC<IPaginationProps> = ({
+  paginationNumbers,
+  updateCurrentPage,
+  currentPage,
+}) => {
+  return (
+    <div className={classes.tablePagination}>
+      {paginationNumbers.map((number) => {
+        return (
+          <p
+            data-test="pagination-paragraph"
+            onClick={() => updateCurrentPage(number)}
+            key={uuid()}
+            className={
+              classes.paginationNumber +
+              " " +
+              (number === currentPage ? classes.active : null)
+            }
+          >
+            {number}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TablePagination;
