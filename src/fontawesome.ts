f845f08cf2e8ee6a5557dfe2262afd96caa53ddf
KacperBiedka <kacper.biedka.00@gmail.com>
2020-11-07 14:27:33 +0100

    feature: TableFilter component
    Additionally setup fontawesome integration

---
+++ src/fontawesome.ts
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+// Import icons separately to avoid large bundle size
+library.add(
+  faChevronDown
+  // more icons go here
+);
---
+++ src/modules/search/components/TableFilter/TableFilter.tsx
@@ -0,0 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { FC } from "react";
+import classes from "./TableFilter.module.scss";
+
+interface IFilter {
+    field: string,
+    name: string,
+    order: string,
+    active: boolean
+}
+
+interface IFilterProps {
+    filter: IFilter,
+    sortData: (index: number) => void,
+    index: number
+}
+
+const TableFilter: FC<IFilterProps> = ({ filter, sortData, index }) => {
+  return (
+    <th
+      data-test="filter-header-cell"
+      onClick={() => sortData(index)}
+      className={filter.order === "asc" ? classes.asc : classes.desc}
+    >
+      {filter.name}{" "}
+      <FontAwesomeIcon
+        data-test="filter-chevron-icon"
+        className={
+          classes.chevron + " " + (filter.active ? classes.active : "")
+        }
+        icon={["fas", "chevron-down"]}
+      />
+    </th>
+  );
+};
+
+export default TableFilter;
