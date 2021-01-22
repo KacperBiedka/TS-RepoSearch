994da1a4d4660793a0e0e4005bc1484886ee6159
KacperBiedka <kacper.biedka.00@gmail.com>
2021-01-22 15:54:06 +0100

    test: e2e repo search table operations

--- cypress/integration/search/search.spec.js
+++ cypress/integration/search/search.spec.js
@@ -5,23 +5,50 @@ describe("RepoSearch page", () => {
         cy.visit('http://localhost:3000');
     });
     it('displays default status info', () => {
-        cy.get('[data-test=empty-results-message]').should('exist');
-        cy.get('[data-test=status-hero]').contains(statusMessages.default);
+        cy.getByDataTest('empty-results-message').should('exist');
+        cy.getByDataTest('status-hero').contains(statusMessages.default);
     });
     context("Initial repository search", () => {
         it("accepts user search input", () => {
-            cy.get('[data-test=repo-search-input]').type('elo');
+            cy.getByDataTest('repo-search-input').type('test');
         });
         it("shows loader", () => {
-            cy.get('[data-test=search-results-loader]').should('exist');
+            cy.getByDataTest('search-results-loader').should('exist');
+        });
+        it("displays correct data after search", () => {
+            cy.getByDataTest('search-results-table').should('exist')
         });
         it("correctly updates url parameters", () => {
             cy.location().should((loc) => {
-                expect(loc.search).eq('?field=name&order=asc&query=elo');
+                expect(loc.search).to.eq('?field=name&order=asc&query=test');
             });
         });
-        it("displays correct data after search", () => {
-            cy.get('[data-test=search-results-table]').should('exist')
+    });
+    context("Table operations", () => {
+        it("accepts user input for entries count", () => {
+            cy.getByDataTest('perPage-input').clear().type(5);
+        })
+        it("displays correct number of rows", () => {
+            cy.getByDataTest("results-table-body-row").should('have.length', 5);
+        })
+        it("updates pagination", () => {
+            cy.getByDataTest("pagination-paragraph").should('have.length', 6);
+        });
+        it("correctly filters data", () => {
+            let oldRepoName;
+            cy.getByDataTest("results-table-body-row").first().then(field => {
+                oldRepoName = field[0].children[0].innerText.toLowerCase();
+            });
+            cy.getByDataTest("filter-header-cell").first().click();
+            cy.getByDataTest("results-table-body-row").first().should(field => {
+                const newRepoName = field[0].children[0].innerText.toLowerCase();
+                expect(newRepoName < oldRepoName).to.equal(true);
+            });
+        });
+        it("updates url parameters after filtering", () => {
+            cy.location().should((loc) => {
+                expect(loc.search).to.eq('?field=name&order=asc&query=test');
+            });
         });
     });
-})
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("getByDataTest", (selector) => {
+    return cy.get(`[data-test=${selector}]`);
+});
