a88063dcecd439c6231fbb8518131f746cbb1944
KacperBiedka <kacper.biedka.00@gmail.com>
2021-01-25 12:10:24 +0100

    test: e2e pagination actions

--- cypress/integration/search/search.spec.js
+++ cypress/integration/search/search.spec.js
@@ -1,5 +1,7 @@
 import { statusMessages } from '../../fixtures/search';
 
+const searchQuery = 'test';
+
 describe("RepoSearch page", () => {
     it('sucessfully loads', () => {
         cy.visit('http://localhost:3000');
@@ -10,17 +12,17 @@ describe("RepoSearch page", () => {
     });
     context("Initial repository search", () => {
         it("accepts user search input", () => {
-            cy.getByDataTest('repo-search-input').type('test');
+            cy.getByDataTest('repo-search-input').type(searchQuery);
         });
         it("shows loader", () => {
             cy.getByDataTest('search-results-loader').should('exist');
         });
-        it("displays correct data after search", () => {
-            cy.getByDataTest('search-results-table').should('exist')
+        it("displays data after search", () => {
+            cy.getByDataTest('search-results-table').should('exist', { timeout: 10000 })
         });
         it("correctly updates url parameters", () => {
             cy.location().should((loc) => {
-                expect(loc.search).to.eq('?field=name&order=desc&query=test');
+                expect(loc.search).to.eq(`?field=name&order=desc&query=${searchQuery}`);
             });
         });
     });
@@ -47,7 +49,18 @@ describe("RepoSearch page", () => {
         });
         it("updates url parameters after filtering", () => {
             cy.location().should((loc) => {
-                expect(loc.search).to.eq('?field=name&order=asc&query=test');
+                expect(loc.search).to.eq(`?field=name&order=asc&query=${searchQuery}`);
+            });
+        });
+        it("shows next data page on pagination click", () => {
+            let oldRepoName;
+            cy.getByDataTest("results-table-body-row").first().then(field => {
+                oldRepoName = field[0].children[0].innerText.toLowerCase();
+            });
+            cy.getByDataTest("pagination-paragraph").last().click();
+            cy.getByDataTest("results-table-body-row").first().should(field => {
+                const newRepoName = field[0].children[0].innerText.toLowerCase();
+                expect(newRepoName > oldRepoName).to.equal(true);
             });
         });
     });
